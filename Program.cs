906dc5bc9dd065a4bd63ec0c14c1db7872e1434b
tylerg05 <tylergardella05@gmail.com>
2020-04-20 18:49:32 -0500

    Option 1 and 2

--- Program.cs
+++ Program.cs
@@ -14,94 +14,108 @@ namespace BlogsConsole
             try
             {
                 var db = new BloggingContext();
-
-                // Ask user for new Blog name
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
-
-                // save blog
-                var newBlog = new Blog { Name = name };
-                db.Blogs.Add(newBlog);
-                db.SaveChanges();
-                logger.Info("new blog saved");
-
-                // Show all blogs
-                var blogs = db.Blogs;
-                Console.WriteLine("Here are all the blogs:");
-                foreach (var item in blogs)
+                var userInput = "1";
+                while (userInput.Equals("1") || userInput.Equals("2") || userInput.Equals("3") || userInput.Equals("4"))
                 {
-                    Console.WriteLine(item.Name);
-                }
+                    Console.WriteLine("1. Display all blogs");
+                    Console.WriteLine("2. Add blog");
+                    Console.WriteLine("3. Create post");
+                    Console.WriteLine("4. Display all posts");
+                    Console.WriteLine("Enter any other character to quit");
+                    userInput = Console.ReadLine();
+                    if (userInput == "1")
+                    {
+                        // Show all blogs
+                        var blogs = db.Blogs;
+                        var blogsAmount = blogs.Count();
+                        Console.WriteLine(blogsAmount + " blogs found");
+                        foreach (var item in blogs)
+                        {
+                            Console.WriteLine(item.Name);
+                        }
 
-                // Ask user to select blog
-                Console.Write("Enter a Blog name: ");
-                var userChoice = Console.ReadLine();
-                var blog = blogs.FirstOrDefault(b => b.Name == userChoice);
+                    }
+                    else if (userInput == "2")
+                    {
+                        // Ask user for new Blog name
+                        Console.Write("Enter a name for a new Blog: ");
+                        var name = Console.ReadLine();
+                        if (name.Equals(""))
+                        {
+                            Console.WriteLine("Blog name cannot be null");
+                        }
+                        else
+                        {
+                            // save blog
+                            var newBlog = new Blog { Name = name };
+                            db.Blogs.Add(newBlog);
+                            db.SaveChanges();
+                            //logger.Info("new blog saved");
+                        }
+                    }
+                    else if (userInput == "3")
+                    {
+                        // Show all blogs
+                        var blogs = db.Blogs;
+                        Console.WriteLine("Here are all the blogs:");
+                        var counter = 1;
+                        foreach (var item in blogs)
+                        {
+                            Console.WriteLine(counter + ") " + item.Name);
+                            counter++;
+                        }
 
-                // Ask user to enter post details
-                Console.WriteLine("Enter a Post");
+                        // Ask user to select blog
+                        Console.Write("Enter a Blog number: ");
+                        var userChoice = Console.ReadLine();
 
-                Console.WriteLine("Enter a title");
-                var title = Console.ReadLine();
+                        // Ask user to enter post details
+                        Console.WriteLine("Enter a Post");
 
-                Console.WriteLine("Enter some content");
-                var content = Console.ReadLine();
+                        Console.WriteLine("Enter a title");
+                        var title = Console.ReadLine();
 
-                // save the post
-                var newPost = new Post
-                {
-                    Title = title,
-                    Content = content,
-                    BlogId = blog.BlogId
-                };
+                        Console.WriteLine("Enter some content");
+                        var content = Console.ReadLine();
 
-                db.Posts.Add(newPost);
-                db.SaveChanges();
-                logger.Info("new post saved");
+                        // save the post
+                        var newPost = new Post
+                        {
+                            Title = title,
+                            Content = content,
+                            BlogId = blog.BlogId
+                        };
 
-                // display the posts
-                Console.WriteLine("Here are the posts:");
-                foreach (var item in db.Posts)
-                {
-                    Console.WriteLine($"title: {item.Title}; content: {item.Content}");
-                }
+                        db.Posts.Add(newPost);
+                        db.SaveChanges();
+                        //logger.Info("new post saved");
+                    }
+                    else if (userInput == "4")
+                    {
+                        // Show all blogs
+                        var blogs = db.Blogs;
+                        Console.WriteLine("Here are all the blogs:");
+                        foreach (var item in blogs)
+                        {
+                            Console.WriteLine(item.Name);
+                        }
 
-                // ask the user to select a post
-                Console.Write("Enter a Post name: ");
-                var postChoice = Console.ReadLine();
-                var post = db.Posts.FirstOrDefault(p => p.Title == postChoice);
+                        // Ask user to select blog
+                        Console.Write("Enter a Blog name: ");
+                        var userChoice = Console.ReadLine();
+                        var blog = blogs.FirstOrDefault(b => b.Name == userChoice);
 
-                // edit the existing post
-                Console.Write("Enter new post content: ");
-                var newContent = Console.ReadLine();
-                post.Title = newContent;
-
-                // save the post
-                db.SaveChanges();
-                logger.Info("posted edited");
-
-                // display the posts again
-                Console.WriteLine("Here are the posts again:");
-                foreach (var item in db.Posts)
-                {
-                    Console.WriteLine($"title: {item.Title}; content: {item.Content}");
-                }
-
-                // delete a post
-                // ask the user to select a post
-                Console.Write("Enter a Post name: ");
-                var postToDelete = Console.ReadLine();
-                var deletePost = db.Posts.FirstOrDefault(p => p.Title == postToDelete);
-
-                db.Posts.Remove(deletePost);
-                db.SaveChanges();
-                logger.Info("posted deleted");
-
-                // display the posts one last time
-                Console.WriteLine("Here are the posts again:");
-                foreach (var item in db.Posts)
-                {
-                    Console.WriteLine($"title: {item.Title}; content: {item.Content}");
+                        // display the posts
+                        Console.WriteLine("Here are the posts:");
+                        foreach (var item in db.Posts)
+                        {
+                            Console.WriteLine($"title: {item.Title}; content: {item.Content}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("The program will now close");
+                    }
                 }
 
             }
